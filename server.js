22ae0acaa85104029cd7641b10d15b4fd169ac2e
Vinícius Augutis <vinicius_augutis@hotmail.com>
2018-09-19 02:17:09 -0300

    #TASK Ajustando configuração do package json :: #DOING

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 3000, function () {
+  console.log("up and running on port " + process.env.PORT);
+});
 
-//
