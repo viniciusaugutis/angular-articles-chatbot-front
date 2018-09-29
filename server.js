31e9dcd18c80e3c01405dc96a6e9ea2027f13df6
Vinícius Augutis <vinicius_augutis@hotmail.com>
2018-09-29 15:39:19 -0300

    #TASK Adicionando dependência para gzip compression e ajustes no server.js :: #RESOLVED

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+var compression = require('compression');
 const express = require('express');
+
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
+app.use(compression());
 app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 });
